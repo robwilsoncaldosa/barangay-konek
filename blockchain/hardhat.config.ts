17282f8bdac18c00e119840acd7a09c98cc3625e
LCuadero <lorencuadero8@gmail.com>
2025-10-17 10:01:51 +0800

    [update] - blockchain hardhat.ts

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -25,20 +25,20 @@
 //   },
 // };
 
+// // export default config;
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.28",
+//   networks: {
+//     sepolia: {
+//       url: process.env.SEPOLIA_RPC_URL || "",
+//       accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
+//     },
+//   },
+// };
+
 // export default config;
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-  networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
-    },
-  },
-};
-
-export default config;
